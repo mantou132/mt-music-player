d1638750992e53a15c7449c726945daf4ee3f5a2
xianqiao.wxq <xianqiao.wxq@alibaba-inc.com>
2019-06-25 20:54:54 +0800

    chore: minify <templete>

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,53 @@
 const path = require('path');
+const { JSDOM } = require('jsdom');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+
+class MyHtmlWebpackPlugin {
+  // Remove `index.js` in `index.html`
+  // eslint-disable-next-line class-methods-use-this
+  apply(compiler) {
+    compiler.hooks.compilation.tap('MyHtmlWebpackPlugin', compilation => {
+      HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
+        'MyHtmlWebpackPlugin',
+        (data, cb) => {
+          const dom = new JSDOM(data.html);
+          const { document } = dom.window;
+          document
+            .querySelectorAll('[href="index.js"],[src="index.js"]')
+            .forEach(ele => ele.remove());
+          data.html = dom.serialize();
+          cb(null, data);
+        },
+      );
+    });
+  }
+}
 
 module.exports = {
   mode: process.env.NODE_ENV,
   entry: './fe/index.js',
   output: {
     path: path.resolve(__dirname, 'fe/dist'),
-    filename: 'index.js',
+    filename: 'index.[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'minify-lit-html-loader',
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MyHtmlWebpackPlugin(),
     new HtmlWebpackPlugin({
-      inject: false,
       template: './fe/index.html',
     }),
     new CopyPlugin([
